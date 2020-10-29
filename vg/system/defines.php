b48b43b042b824598434de4155b73cc6c7cdafe9
splinter44 <73307226+splinter44@users.noreply.github.com>
2020-10-29 16:38:08 +0200

    Update defines.php

--- vg/system/defines.php
+++ vg/system/defines.php
@@ -8,7 +8,7 @@ define("_ADMIN_EMAIL_", "6216326@stud.nau.edu.ua");
  * Настройки почты по умолчанию
  */
 define('_MAIL_TYPE_', 'SMTP');
-define('_SMTP_HOST_', 'smtp.nau.edu.ua');
+define('_SMTP_HOST_', 'mail.nau.edu.ua');
 define('_SMTP_LOGIN_', 'splinter4234@gmail.com'); // email с которого будут отправлятся письма
 define('_SMTP_PASSWORD_', '626384315253F'); // пароль от почты с которой будут отправлятся письма
 define('_SMTP_SECURE_', 'ssl'); // tls
